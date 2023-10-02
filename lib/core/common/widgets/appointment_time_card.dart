ec14da26f57b88c3df57356e1bd0be58f60c7b8d
Sanyog Singh Rajput <sanyogsingrajput@gmail.com>
2023-10-03 01:31:58 +0530

    fixed home responsiveness

--- lib/core/common/widgets/appointment_time_card.dart
+++ lib/core/common/widgets/appointment_time_card.dart
@@ -1,55 +1,135 @@
+// import 'package:flutter/material.dart';
+// import 'package:hairon/core/styling/theme/style.dart';
+
+// class AppointmentTimeCard extends StatelessWidget{
+//   final bool? isMarginNeeded;
+//   final Color color;
+//   AppointmentTimeCard({Key? key,  this.isMarginNeeded, required this.color}): super(key: key);
+
+//   @override
+//   Widget build(BuildContext context){
+//     return  Container(
+//       margin: isMarginNeeded == true ? EdgeInsets.only(bottom: 10, ):  null
+
+//            ,
+//       height: MediaQuery.of(context).size.height*0.1,
+//       width: MediaQuery.of(context).size.width,
+//       decoration: BoxDecoration(
+//           color: color,
+//           borderRadius: BorderRadius.circular(8)),
+//       child: Row(
+//         children: [
+//           Container(
+//             margin: EdgeInsets.only(
+//                 top: 20, left: 30, bottom: 20),
+//             height: 55,
+//             width: 50,
+//             decoration: BoxDecoration(
+//                 color: primaryColor,
+//                 shape: BoxShape.circle),
+//             child: Icon(
+//               Icons.calendar_today,
+//               color: whiteColor,
+//               size: 20,
+//             ),
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.only(
+//                 top: 23, left: 20),
+//             child: Column(
+//               crossAxisAlignment:
+//               CrossAxisAlignment.start,
+//               children: [
+//                 Text(
+//                   'Tommorow at 12:30',
+//                   style: TextStyle(
+//                       fontSize: 16,
+//                       color: primaryColor,
+//                       fontWeight: FontWeight.w800),
+//                 ),
+//                 SizedBox(
+//                   height: 4,
+//                 ),
+//                 Row(
+//                   children: [
+//                     Text(
+//                       'Hairstyling at ',
+//                       style: TextStyle(),
+//                     ),
+//                     Text(
+//                       'Salon Name',
+//                       style: TextStyle(
+//                           fontWeight: FontWeight.bold),
+//                     )
+//                   ],
+//                 )
+//               ],
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:hairon/core/styling/theme/style.dart';
 
-class AppointmentTimeCard extends StatelessWidget{
+class AppointmentTimeCard extends StatelessWidget {
   final bool? isMarginNeeded;
   final Color color;
-  AppointmentTimeCard({Key? key,  this.isMarginNeeded, required this.color}): super(key: key);
+
+  AppointmentTimeCard({Key? key, this.isMarginNeeded, required this.color})
+      : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return  Container(
-      margin: isMarginNeeded == true ? EdgeInsets.only(bottom: 10, ):  null
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
 
-           ,
-      height: 100,
-      width: MediaQuery.of(context).size.width,
+    final cardHeight = screenHeight * 0.12;
+
+    return Container(
+      margin: isMarginNeeded == true ? EdgeInsets.only(bottom: 10) : null,
+      padding:
+          EdgeInsets.symmetric(vertical: 16, horizontal: screenWidth * 0.05),
+      width: double.infinity,
+      height: cardHeight, // Set the card's height
       decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(8)),
+        color: color,
+        borderRadius: BorderRadius.circular(8),
+      ),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
-            margin: EdgeInsets.only(
-                top: 20, left: 30, bottom: 20),
             height: 55,
             width: 50,
             decoration: BoxDecoration(
-                color: primaryColor,
-                shape: BoxShape.circle),
+              color: primaryColor,
+              shape: BoxShape.circle,
+            ),
             child: Icon(
               Icons.calendar_today,
               color: whiteColor,
               size: 20,
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(
-                top: 23, left: 20),
+          SizedBox(width: screenWidth * 0.05),
+          Expanded(
             child: Column(
-              crossAxisAlignment:
-              CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Tommorow at 12:30',
+                  'Tomorrow at 12:30',
                   style: TextStyle(
-                      fontSize: 16,
-                      color: primaryColor,
-                      fontWeight: FontWeight.w800),
+                    fontSize: 16,
+                    color: primaryColor,
+                    fontWeight: FontWeight.w800,
+                  ),
                 ),
-                SizedBox(
-                  height: 4,
-                ),
+                SizedBox(height: 3),
                 Row(
                   children: [
                     Text(
@@ -58,14 +138,13 @@ class AppointmentTimeCard extends StatelessWidget{
                     ),
                     Text(
                       'Salon Name',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold),
-                    )
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
                   ],
-                )
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
--- lib/features/Home/presentation/pages/home_screen.dart
+++ lib/features/Home/presentation/pages/home_screen.dart
@@ -1,129 +1,241 @@
+// import 'package:flutter/material.dart';
+// import 'package:hairon/core/common/widgets/appointment_time_card.dart';
+// import 'package:hairon/features/Home/presentation/widgets/homescreen/below_header_bar_widget.dart';
+// import 'package:hairon/features/Home/presentation/widgets/homescreen/header_bar_widget.dart';
+// import 'package:hairon/core/styling/theme/style.dart';
+// // import 'dart:js_util';
+
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
+
+//   @override
+//   State<HomeScreen> createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         backgroundColor: backgroundColor,
+//         body: SafeArea(
+
+//               child: Stack( children: [
+//                     Positioned(
+//                         child: Column(
+//                       children: [
+//                         HeaderBarWidget(),
+//                         SizedBox(
+//                           height: 70,
+//                         ),
+//                         BelowHeaderBarWidget(
+//                           name: "Kunal",
+//                         ),
+//                       ],
+//                     )),
+//                     Positioned(
+//                         right: 0,
+//                         bottom: 0,
+//                         left: 0,
+//                         child: Container(
+//                           height: MediaQuery.of(context).size.height * 0.5,
+//                           decoration: BoxDecoration(
+//                               color: whiteColor,
+//                               borderRadius: BorderRadius.only(
+//                                 topLeft: Radius.circular(45),
+//                                 topRight: Radius.circular(45),
+//                               )),
+//                           child: Padding(
+//                               padding: EdgeInsets.only(
+//                                   top: 50, left: 20, right: 20, bottom: 10),
+//                               child: Column(
+//                                 children: [
+//                                   Row(
+//                                     mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                                     children: [
+//                                       _serviceItem(
+//                                           height: 40,
+//                                           width: 40,
+//                                           serviceImage: "assets/women_hairon.png",
+//                                           serviceName: "Women"),
+//                                       _serviceItem(
+//                                           height: 40,
+//                                           width: 40,
+//                                           serviceImage: "assets/man.png",
+//                                           serviceName: "Men"),
+//                                       _serviceItem(
+//                                           height: 40,
+//                                           width: 40,
+//                                           serviceImage: "assets/women_hairon.png",
+//                                           serviceName: "Women"),
+//                                     ],
+//                                   ),
+//                                   SizedBox(
+//                                     height: 60,
+//                                   ),
+//                                   Row(
+//                                     mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                                     children: [
+//                                       _serviceItem(
+//                                           height: 40,
+//                                           width: 40,
+//                                           serviceImage: "assets/women_hairon.png",
+//                                           serviceName: "Women"),
+//                                       _serviceItem(
+//                                           height: 40,
+//                                           width: 40,
+//                                           serviceImage: "assets/women_hairon.png",
+//                                           serviceName: "Women"),
+//                                       _serviceItem(
+//                                           height: 40,
+//                                           width: 40,
+//                                           serviceImage: "assets/women_hairon.png",
+//                                           serviceName: "Women"),
+//                                     ],
+//                                   ),
+//                                   SizedBox(
+//                                     height: 60,
+//                                   ),
+//                                  AppointmentTimeCard(color: fadeColor,)
+//                                 ],
+//                               )),
+//                         )),
+//                   ],
+//                 ),
+//             ),
+
+//         );
+//   }
+
+//   Widget _serviceItem(
+//       {required String serviceImage,
+//       required String serviceName,
+//       required double width,
+//       required double height}) {
+//     return Column(
+//       children: [
+//         Image.asset(
+//           "$serviceImage",
+//           scale: 5,
+//           width: width,
+//           height: height,
+//         ),
+//         SizedBox(
+//           height: 8,
+//         ),
+//         Text(
+//           '$serviceName',
+//           style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+//         )
+//       ],
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:hairon/core/common/widgets/appointment_time_card.dart';
 import 'package:hairon/features/Home/presentation/widgets/homescreen/below_header_bar_widget.dart';
 import 'package:hairon/features/Home/presentation/widgets/homescreen/header_bar_widget.dart';
 import 'package:hairon/core/styling/theme/style.dart';
-// import 'dart:js_util';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-        backgroundColor: backgroundColor,
-        body: SafeArea(
-          child: Stack(
-            children: [
-              Positioned(
+      backgroundColor: backgroundColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            HeaderBarWidget(),
+            SizedBox(height: screenHeight * 0.1),
+            BelowHeaderBarWidget(
+              name: "Kunal",
+            ),
+            SizedBox(
+              height: screenHeight * 0.07,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: whiteColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(45),
+                    topRight: Radius.circular(45),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: screenHeight * 0.05, horizontal: 20),
                   child: Column(
-                children: [
-                  HeaderBarWidget(),
-                  SizedBox(
-                    height: 70,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          _serviceItem(
+                            serviceImage: "assets/women_hairon.png",
+                            serviceName: "Women",
+                          ),
+                          _serviceItem(
+                            serviceImage: "assets/man.png",
+                            serviceName: "Men",
+                          ),
+                          _serviceItem(
+                            serviceImage: "assets/women_hairon.png",
+                            serviceName: "Women",
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: screenHeight * 0.02),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          _serviceItem(
+                            serviceImage: "assets/women_hairon.png",
+                            serviceName: "Women",
+                          ),
+                          _serviceItem(
+                            serviceImage: "assets/women_hairon.png",
+                            serviceName: "Women",
+                          ),
+                          _serviceItem(
+                            serviceImage: "assets/women_hairon.png",
+                            serviceName: "Women",
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: screenHeight * 0.02),
+                      AppointmentTimeCard(color: fadeColor),
+                    ],
                   ),
-                  BelowHeaderBarWidget(
-                    name: "Kunal",
-                  ),
-                ],
-              )),
-              Positioned(
-                  right: 0,
-                  bottom: 0,
-                  left: 0,
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.5,
-                    decoration: BoxDecoration(
-                        color: whiteColor,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(45),
-                          topRight: Radius.circular(45),
-                        )),
-                    child: Padding(
-                        padding: EdgeInsets.only(
-                            top: 50, left: 20, right: 20, bottom: 10),
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                _serviceItem(
-                                    height: 40,
-                                    width: 40,
-                                    serviceImage: "assets/women_hairon.png",
-                                    serviceName: "Women"),
-                                _serviceItem(
-                                    height: 40,
-                                    width: 40,
-                                    serviceImage: "assets/man.png",
-                                    serviceName: "Men"),
-                                _serviceItem(
-                                    height: 40,
-                                    width: 40,
-                                    serviceImage: "assets/women_hairon.png",
-                                    serviceName: "Women"),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 60,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                _serviceItem(
-                                    height: 40,
-                                    width: 40,
-                                    serviceImage: "assets/women_hairon.png",
-                                    serviceName: "Women"),
-                                _serviceItem(
-                                    height: 40,
-                                    width: 40,
-                                    serviceImage: "assets/women_hairon.png",
-                                    serviceName: "Women"),
-                                _serviceItem(
-                                    height: 40,
-                                    width: 40,
-                                    serviceImage: "assets/women_hairon.png",
-                                    serviceName: "Women"),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 60,
-                            ),
-                           AppointmentTimeCard(color: fadeColor,)
-                          ],
-                        )),
-                  )),
-            ],
-          ),
-        ));
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
-  Widget _serviceItem(
-      {required String serviceImage,
-      required String serviceName,
-      required double width,
-      required double height}) {
+  Widget _serviceItem({
+    required String serviceImage,
+    required String serviceName,
+  }) {
     return Column(
       children: [
         Image.asset(
-          "$serviceImage",
+          serviceImage,
           scale: 5,
-          width: width,
-          height: height,
+          width: 40,
+          height: 40,
         ),
-        SizedBox(
-          height: 8,
-        ),
+        SizedBox(height: 8),
         Text(
-          '$serviceName',
+          serviceName,
           style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-        )
+        ),
       ],
     );
   }
